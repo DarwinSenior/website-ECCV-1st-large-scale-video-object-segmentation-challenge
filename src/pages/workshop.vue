c85adb101d46352dd7b3eda9a0845c07168b2597
dingcheng <darwinsenior@gmail.com>
2018-05-14 20:25:53 +0100

    adding missing pages

---
+++ src/pages/workshop.vue
@@ -0,0 +1,30 @@
+<style lang="scss" scoped>
+</style>
+
+<template lang="pug">
+#page-workshop
+  section.section#display-section: .container
+    .section
+      h1.title The 1st Large-scale Video Object Segmentation Challenge
+    .section
+      h1.title.is-5 Invited Speakers
+    .section
+      h1.title.is-5 Workshop Program (Sep 14th afternoon)
+      p Will be available soon
+
+    
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class WorkshopPage extends Vue {
+
+  organizers = [
+    {name:'Ning Xu', affiliation:'Adobe Research', im_name:'Ning_Xu'},
+    {name:'Linjie Yang', affiliation:'Snap Research', im_name:'LinjieYang'},
+    {name:'Yuchen Fan', affiliation:'UIUC', im_name:'YuchenFan'},
+  ]
+}
+</script>
